fb38235122d748b9d89cae1ba4718854aba94951
Maung Ko Ko <mwkoko2020@gmail.com>
2023-06-04 13:06:07 -0600

    Complete Lab

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,27 @@
 // Code Keypad Component Here
+import React from 'react';
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+  const handleChange = (event) => {
+    console.log('Entering password...');
+    console.log(event.target.value);
+  };
+  return (
+    <div>
+      <input
+        type="password"
+        name="password"
+        onChange={handleChange}
+        placeholder="Enter password..."
+      />
+
+      <select name="filter" onChange={handleChange}>
+        <option value="all">Select a filter...</option>
+        <option value="completed">Completed</option>
+        <option value="incomplete">Incomplete</option>
+      </select>
+    </div>
+  );
 }
 
 export default Keypad;
